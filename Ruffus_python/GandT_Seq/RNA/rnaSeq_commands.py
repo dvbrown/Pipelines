379a8c0cf9b910c790c5e0a7bd893a2c6c54a72e
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-06-17 11:20:19 +0100

    created new pipeline for RNA-Seq data

---
+++ Ruffus_python/GandT_Seq/RNA/rnaSeq_commands.py
@@ -0,0 +1,112 @@
+import os, time, re
+
+#################################    GLOBAL PARAMETERS    #####################################
+
+refGenome = '/uz/data/avalok/symbiosys/gcpi_r_kul_thierry_voet/dbrown0/Data/hg19Mt'
+# NEED a reference with the ERCC sequences!!!
+
+binaryPath = '/cm/shared/apps/'
+javaPath = '/cm/shared/apps/jdk/1.7.0/bin/java'
+picardPath = '/cm/shared/apps/picard/current/'
+gatkPath = '/cm/shared/apps/gatk/current/'
+bedtoolsPath = '/cm/shared/apps/bedtools/2.17.0/bin/'
+localBinaryPath = '/home/dbrown0/.local/bin/'
+tmpDir = ''
+
+#################################    BEGIN COMMANDS    #####################################
+
+def runJob(comm, taskName):
+    '''An internal function used by the rest of the functions to spawn a process in the shell, 
+    run a command based on the value of the string "comm" and capture the standard output.
+    Throws an exception when failure occurs'''
+    started = time.strftime('%X %x %Z')
+    print('\n##############################################    RUNNNG TASK ' + taskName + ' at {0}'.format(started) +   '    ###############################################')
+    print(comm + '\n')
+    #run the command. Comment out the line below to print only the command and not run it.
+    os.system(comm)
+    
+    
+def trimReads(inputFile, outputFile):
+    'Take the raw sequencing reads and trim off the adpaters'
+    read2 = re.sub('.R1.fastq.gz', '.R2.fastq.gz', inputFile)
+    outputFile2 = re.sub('', '', outputFile)
+    #	Trim the Nextera adapter sequences
+    comm = '''{5}cutadapt -q 15,15 --minimum-length 35 \
+    -a CTGTCTCTTATA -A CTGTCTCTTATA \
+    -o {3} -p {4} {1} {2} \
+    '''.format(binaryPath, inputFile, read2, outputFile, outputFile2, localBinaryPath)
+    runJob(comm, 'TRIMMING READS')
+    
+
+def alignReads(inputFile, outputFile):
+    '''Align the fastq reads using bowtie'''
+    #	Extract the read 2 filename
+    read2 = re.sub('.R1.fastq.gz', '.R2.fastq.gz', inputFile)
+    sampleName = inputFile[90:162]
+    libraryName = inputFile[117:136]
+    #   Build the read group information
+    rgID = '{0} --rg SM:{1} --rg PL:ILLUMINA --rg LB:{2} '.format(inputFile, libraryName, sampleName)
+    comm = '''{0}bowtie2/current/bowtie2 --local -p 8 --rg-id {1} -x {2} -1 {3} -2 {4} \
+    | {0}samtools/current/samtools view -bS -o {5} -S \
+    '''.format(binaryPath, rgID, refGenome, inputFile, read2, outputFile)
+    runJob(comm, 'ALIGNING READS')
+    
+    
+def mergeBamPipeline(inputFileNames, outputFile):
+    'As some samples are split over 4 lanes of sequencing on a NextSeq combine the aligned files'
+    bam1 = inputFileNames[0]
+    bam2 = inputFileNames[1]
+    bam3 = inputFileNames[2]
+    bam4 = inputFileNames[3]
+    comm = '''java -Xmx5g -jar {0}MergeSamFiles.jar \
+    INPUT={1} INPUT={2} INPUT={3} INPUT={4} \
+    OUTPUT={5} SORT_ORDER=coordinate CREATE_INDEX=true \
+    '''.format(picardPath, bam1, bam2, bam3, bam4, outputFile, tmpDir)
+    runJob(comm, 'MERGING BAM FILES')
+    
+    
+def sortSamtools(inputFile, outputFile):
+    comm = "{0}samtools/current/samtools sort {1} > {2}".format(binaryPath, inputFile, outputFile)
+    runJob(comm, "SORTING ALIGNMENTS")
+    
+
+def indexSamtools(inputFile):
+    com = "{0}samtools/current/samtools index {1}".format(binaryPath, inputFile)
+    # No output file therefore invoke os.system directly
+    print('\n##############################################    RUNNNG TASK INDEX SAMTOOLS     ###############################################\n')
+    os.system(com)
+    
+    
+def estimateLibComplexity(inputFile, outputFile):
+    'Estimate the library size using Picard tools. This should be done before duplicate removal'
+    comm = '''java -Xmx5g -jar {0}EstimateLibraryComplexity.jar \
+     I={1} \
+     O={2}'''.format(picardPath, inputFile, outputFile)
+    runJob(comm, 'ESTIMATING LIBRARY SIZE')
+    
+    
+def countAlignChr(inputFile, outputFile):
+    'Obtain the number of reads mapping to each chromosome'
+    comm = '''samtools idxstats {0} | cut -f 1,3 > {1}'''.format(inputFile, outputFile)
+    runJob(comm, 'COUNT READS PER CHROMOSOME')
+    
+    
+def collectInsertSize(inputFile, outputFile):
+    'Generate a plot of insert size distribution using Picard'
+    comm = '''java -Xmx5g -jar {0}CollectInsertSizeMetrics.jar \
+    I={1} O={2}.txt H={2}.pdf \
+    M=0.5'''.format(picardPath, inputFile, outputFile)
+    runJob(comm, 'GENERATE INSERT SIZE PLOT')
+
+def calculateExpression(inputFile, outputFile):
+    'Calculate the expression estimate in TPM using RSEM'
+    comm = '''
+    '''
+    runJob(comm, 'CALUCLATING EXPRESSION')
+    
+    
+def countReadsGenes(inputFile, outputFile):
+    'Count the number of reads mapped to genes using HTSeq'
+    comm = '''
+    '''
+    runJob(comm, 'COUNTING READS TO GENES')
---
+++ Ruffus_python/GandT_Seq/RNA/rnaSeq_pipeline.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+"""
+
+    ruffus_template.py  [--input_file]
+                        [--log_file PATH]
+                        [--verbose]
+                        [--target_tasks]
+                        [--jobs]
+                        [--just_print]
+                        [--flowchart]
+                        [--key_legend_in_graph]
+                        [--forced_tasks]
+
+"""
+import sys, os, re
+import rnaSeq_commands 
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   options
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    import StringIO
+
+    parser = OptionParser(version="%prog 1.0", usage = "\n\n    %progs [options]")
+
+    #
+    #   general options: verbosity / logging
+    #
+    parser.add_option("-v", "--verbose", dest = "verbose",
+                      action="count", default=1,
+                      help="Print more verbose messages for each additional verbose level.")
+    parser.add_option("-L", "--log_file", dest="log_file",
+                      metavar="FILE",
+                      type="string",
+                      help="Name and path of log file")
+
+    #
+    #   pipeline options
+    #
+    parser.add_option("-i", "--input_file", dest="input_file",
+                        action="append",
+                        default = list(),
+                        metavar="FILE",
+                        type="string",
+                        help="""Write some help""")                             
+    parser.add_option("-t", "--target_tasks", dest="target_tasks",
+                        action="append",
+                        default = list(),
+                        metavar="JOBNAME",
+                        type="string",
+                        help="Target task(s) of pipeline.")
+    parser.add_option("-j", "--jobs", dest="jobs",
+                        default=1,
+                        metavar="N",
+                        type="int",
+                        help="Allow N jobs (commands) to run simultaneously.")
+    parser.add_option("-n", "--just_print", dest="just_print",
+                        action="store_true", default=False,
+                        help="Don't actually run any commands; just print the pipeline.")
+    parser.add_option("--flowchart", dest="flowchart",
+                        metavar="FILE",
+                        type="string",
+                        help="Don't actually run any commands; just print the pipeline "
+                             "as a flowchart.")
+    #
+    #   Less common pipeline options
+    #
+    parser.add_option("--key_legend_in_graph", dest="key_legend_in_graph",
+                        action="store_true", default=False,
+                        help="Print out legend and key for dependency graph.")
+    parser.add_option("--forced_tasks", dest="forced_tasks",
+                        action="append",
+                        default = list(),
+                        metavar="JOBNAME",
+                        type="string",
+                        help="Pipeline task(s) which will be included even if they are up to date.")
+
+    # get help string
+    f =StringIO.StringIO()
+    parser.print_help(f)
+    helpstr = f.getvalue()
+    (options, remaining_args) = parser.parse_args()
+
+
+    #vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    #                                             #
+    #   Change this if necessary                  #
+    #                                             #
+    #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+    #
+    #   Add names of mandatory options,
+    #       strings corresponding to the "dest" parameter
+    #       in the options defined above
+    #
+    mandatory_options = [ ]
+
+    #vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    #                                             #
+    #   Change this if necessary                  #
+    #                                             #
+    #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+    def check_mandatory_options (options, mandatory_options, helpstr):
+        """
+        Check if specified mandatory options have b een defined
+        """
+        missing_options = []
+        for o in mandatory_options:
+            if not getattr(options, o):
+                missing_options.append("--" + o)
+
+        if not len(missing_options):
+            return
+
+        raise Exception("Missing mandatory parameter%s: %s.\n\n%s\n\n" %
+                        ("s" if len(missing_options) > 1 else "",
+                         ", ".join(missing_options),
+                         helpstr))
+    check_mandatory_options (options, mandatory_options, helpstr)
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   imports
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+from ruffus import *
+import subprocess
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   Functions
+
+def run_cmd(cmd_str):
+    """
+    Throw exception if run command fails
+    """
+    process = subprocess.Popen(cmd_str, stdout = subprocess.PIPE,
+                                stderr = subprocess.PIPE, shell = True)
+    stdout_str, stderr_str = process.communicate()
+    if process.returncode != 0:
+        raise Exception("Failed to run '%s'\n%s%sNon-zero exit status %s" %
+                            (cmd_str, stdout_str, stderr_str, process.returncode))
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   Logger
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+import logging
+logger = logging.getLogger(options.target_tasks)
+#
+# We are interesting in all messages
+#
+if options.verbose:
+    logger.setLevel(logging.DEBUG)
+    stderrhandler = logging.StreamHandler(sys.stderr)
+    stderrhandler.setFormatter(logging.Formatter("    %(message)s"))
+    stderrhandler.setLevel(logging.DEBUG)
+    logger.addHandler(stderrhandler)
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   End preamble, begin pipeline 
+#   The code below is from Josien sge batch
+#   sge batch -e errorfiles/qc1 -o outputfiles/qc1 -N QC1 sh shfiles/qc1.sh\
+#   output /uz/data/avalok/symbiosys/gcpi_r_kul_thierry_voet/dbrown0/Data/ATAC-Seq
+
+#################################    PIPELINE CODE GOES HERE    #####################################
+
+# Assign the input specifed from the command line to a variable
+inputFile = options.input_file
+
+@transform(inputFile, suffix('.fastq.gz'), '.bam')
+def runAlignment(inputFile, outputFile):
+    rnaSeq_commands.alignReads(inputFile, outputFile)
+    
+@transform(inputFile, suffix('.bam'), '.merge.bam')
+def runBamMerge(inputFile, outputFile):
+    rnaSeq_commands.mergeBams(inputFile, outputFile)
+    
+#   Align the fastqs from each lane in a single script
+ @transform(inputFile, suffix('lane1.gcap_dev.R1.fastq.gz'), 'lane1.gcap_dev.R1.bam')
+ def runAligLane1(inputFile, outputFile):
+     rnaSeq_commands.alignReads(inputFile, outputFile)
+ 
+ # Make this a follows decorator
+ @follows(runAligLane1) 
+ @transform(inputFile, suffix('lane2.gcap_dev.R1.fastq.gz'), 'lane2.gcap_dev.R1.bam')
+ def runAligLane2(inputFile, outputFile):
+     rnaSeq_commands.alignReads(inputFile, outputFile)
+ 
+ @follows(runAligLane2) 
+ @transform(inputFile, suffix('lane3.gcap_dev.R1.fastq.gz'), 'lane3.gcap_dev.R1.bam')
+ def runAligLane3(inputFile, outputFile):
+     rnaSeq_commands.alignReads(inputFile, outputFile)
+ 
+ @follows(runAligLane3)
+ @transform(inputFile, suffix('lane4.gcap_dev.R1.fastq.gz'), 'lane4.gcap_dev.R1.bam')
+ def runAligLane4(inputFile, outputFile):
+     rnaSeq_commands.alignReads(inputFile, outputFile)
+ 
+ #   Merge all bams from different lanes together into one file
+ #   Generate output file name. Make this using grep next time
+ mergeName = inputFile[0]
+ mergeName = mergeName[91:116]
+ 
+ @follows(runAligLane4)  
+ @merge([runAligLane1, runAligLane2, runAligLane3, runAligLane4], '{0}.merge.bam'.format(mergeName))
+ def runBamMergePipeline(inputFileNames, outputFile):
+     rnaSeq_commands.mergeBamPipeline(inputFileNames, outputFile)
+    
+#-------------------------    POST ALIGNMENT    -----------------------------
+
+@transform(runBamMergePipeline, suffix('.bam'), '.exp.txt')
+def runCalculateExpression(inputFileNames, outputFile):
+    rnaSeq_commands.calculateExpression(inputFileNames, outputFile)
+
+@follows(runBamMergePipeline, suffix('.bam'), '.count.txt')
+@transform(inputFileNames, outputFile):
+    rnaSeq_commands.countReadsGenes(inputFileNames, outputFile)
+    
+#################################    END PIPELINE    #####################################
+
+#   Print list of tasks
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+if options.just_print:
+    pipeline_printout(sys.stdout, options.target_tasks, verbose=options.verbose)
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   Print flowchart
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+elif options.flowchart:
+    # use file extension for output format
+    output_format = os.path.splitext(options.flowchart)[1][1:]
+    pipeline_printout_graph (open(options.flowchart, "w"),
+                             output_format,
+                             options.target_tasks,
+                             no_key_legend = True)
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   Run Pipeline
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+else:
+    pipeline_run(options.target_tasks,  multiprocess = options.jobs,
+                        logger = logger, verbose=options.verbose)
