4b1387ab93d9dbc47ac4fa97c487cf57df73282f
Daniel <daniel.brown@cme.vib-kuleuven.be>
2016-09-15 18:05:57 +0200

    sebastiaan picked up my error

--- Ruffus_python/ATAC-Seq/atac_commands.py
+++ Ruffus_python/ATAC-Seq/atac_commands.py
@@ -48,6 +48,9 @@ def alignReads(inputFile, outputFile):
     libraryName = inputFile[117:136]
     #   Build the read group information
     rgID = '{0} --rg SM:{1} --rg PL:ILLUMINA --rg LB:{2} '.format(inputFile, libraryName, sampleName)
+    
+    # Sebastiaan recommends I use the -X 2000 parameter as per the Buenrostro paper. I forgot to add this
+    
     comm = '''{0}bowtie2/current/bowtie2 --local -p 8 --rg-id {1} -x {2} -1 {3} -2 {4} \
     | {0}samtools/current/samtools view -bS -o {5} -S \
     '''.format(binaryPath, rgID, refGenome, inputFile, read2, outputFile)
