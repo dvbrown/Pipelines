fcc47c5c7eb2e073999c8cbd3dc64186e4dbe1bb
DanBrownHome <dvbro0@gmail.com>
2019-02-10 21:04:30 +1100

    make script

--- bash/190210_downsampleBam.sh
+++ bash/190210_downsampleBam.sh
@@ -1,4 +1,9 @@
-frac=$( samtools idxstats input.bam | cut -f3 | awk 'BEGIN {total=0} {total += $1} \
+#!/bin/bash
+
+inputBam=/
+outputBam=/
+
+frac=$( samtools idxstats $inputBam | cut -f3 | awk 'BEGIN {total=0} {total += $1} \
 END {frac=15000000/total; if (frac > 1) {print 1} else {print frac}}' )
 
-samtools view -bs $frac input.bam > subsample.bam
+samtools view -bs $frac $inputBam > $outputBam
