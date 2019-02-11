4160291cd8883b967ccddbbba287d2c429007818
DanBrown <brown.d@wehi.edu.au>
2019-02-11 16:17:40 +1100

    new torqueLord scripts

--- bash/torqueLord/cellranger_CITEseq.sh
+++ bash/torqueLord/cellranger_CITEseq.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=2,mem=8gb,walltime=00:10:00
-
 source activate tenX
-
-cellrangerExec=/home/brown.d/src/cellranger-3.0.2/cellranger
-transcriptomeRef=/wehisan/general/user_managed/grpu_naik.s_2/Ref_Genomes/refdata-cellranger-mm10-1.2.0
-libraries=/home/brown.d/src/CITEseq/librariesCITE.csv
-featRef=/home/brown.d/src/CITEseq/citeSeqReference_Dec2018.csv
+cellrangerExec=/stornext/HPCScratch/home/brown.d/src/cellranger-3.0.2/cellranger
+transcriptomeRef=/wehisan/general/user_managed/grpu_naik.s_2/TW/Ref_Genomes/refdata-cellranger-mm10-1.2.0
+libraries=/stornext/HPCScratch/home/brown.d/src/CITEseq/librariesCITE.csv
+featRef=/stornext/HPCScratch/home/brown.d/src/CITEseq/citeSeqReference_Dec2018.csv
 
 $cellrangerExec count --id=dan_CITEseq \
                    --transcriptome=$transcriptomeRef \
                    --libraries=$libraries --feature-ref=$featRef \
-                   --expect-cells=1000
+                   --chemistry=SC3Pv2 \
+                   --expect-cells=30000
