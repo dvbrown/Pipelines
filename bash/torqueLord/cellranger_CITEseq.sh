78eeae30945e922f0d922e9eecf93673791ea084
DanBrown <brown.d@wehi.edu.au>
2019-02-11 17:33:28 +1100

    update

--- bash/torqueLord/cellranger_CITEseq.sh
+++ bash/torqueLord/cellranger_CITEseq.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source activate tenX
 cellrangerExec=/stornext/HPCScratch/home/brown.d/src/cellranger-3.0.2/cellranger
 transcriptomeRef=/wehisan/general/user_managed/grpu_naik.s_2/TW/Ref_Genomes/refdata-cellranger-mm10-1.2.0
 libraries=/stornext/HPCScratch/home/brown.d/src/CITEseq/librariesCITE.csv
